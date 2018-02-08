74e56c69a358a41bd4c0cb2f5ebe8d95c05dc012
matsumoto-pac <matsumoto@pactree.jp>
2018-02-08 19:02:43 +0900

    ぱどバリュー作業中

---
+++ page_value.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: ぱどバリュー
+*/
+?>
+<?php get_header(); ?>
+<article id="value" class="underlayer">
+  <section id="sec_01" class="sec">
+    <div class="cover">
+
+    </div>
+  </section>
+  <section id="sec_02" class="sec">
+    <h2 class="ttl_en-ja align_center">
+        <span class="en">PADO&nbsp;VALUE</span>
+        <span class="ja">ぱどバリュー</span>
+      </h2>
+      <div class="desc align_center">
+        <p>
+          ぱどの事業を推進してくうえで、会社が社員の皆さんと共有したい基本姿勢を「ぱどバリュー」<br>
+          として定めます(内容は適宜見直し、今会社が皆さんと一番共有したいものを設定します)。<br>
+          皆さんがこの「ぱどバリュー」を常に意識し、ぱど社員としての価値観を体現しながら<br>
+          日々の業務に取り組むことで、会社と社員が同じ方向を向いて成長していくことができます。
+        </p>
+
+      </div>
+        <div class="vision">
+          <p>人・街・元気</p>
+          <p>情報を通じて 人と人 人と街をつなぎ 人も街も元気にする</p>
+        </div>
+        <p>「顧客志向」を重視しつつ、 ぱどバリュー(基本姿勢)を大事にして実施</p>
+
+  </section>
+  <section id="sec_03" class="sec">
+    <div class="cont col_2 col_2-right">
+      <div>
+        <h3></h3>
+        <ul >
+          <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_01.png" alt="自主・自律"><figcaption>自ら気づき、自ら考え、<br>自ら行動する</figcaption></figure></li>
+          <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_02.png" alt="信頼・尊敬・感謝"><figcaption>相手に対する信頼・尊敬・<br>感謝を忘れず協力しあう</figcaption></figure></li>
+          <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_03.png" alt="チャレンジ"><figcaption>常に変化を恐れずに挑戦を続け、事業拡大のエンジンとなる</figcaption></figure></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  </article>
+  <?php get_template_part( 'template-parts/content-entry' ); ?>
+<?php get_footer(); ?>
