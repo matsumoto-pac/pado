7709566ffe8d076659f65f5f8f8beaf8fc2d385b
matsumoto-pac <matsumoto@pactree.jp>
2018-02-09 19:56:39 +0900

    PC完了

--- page_value.php
+++ page_value.php
@@ -16,7 +16,7 @@ Template Name: ぱどバリュー
         <span class="ja">ぱどバリュー</span>
       </h2>
       <div class="desc align_center">
-        <p>
+        <p class="pado_txt">
           ぱどの事業を推進してくうえで、会社が社員の皆さんと共有したい基本姿勢を「ぱどバリュー」<br>
           として定めます(内容は適宜見直し、今会社が皆さんと一番共有したいものを設定します)。<br>
           皆さんがこの「ぱどバリュー」を常に意識し、ぱど社員としての価値観を体現しながら<br>
@@ -32,13 +32,12 @@ Template Name: ぱどバリュー
 
   </section>
   <section id="sec_03" class="sec">
-    <div class="cont col_2 col_2-right">
+    <div class="cont">
       <div>
-        <h3></h3>
         <ul >
           <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_01.png" alt="自主・自律"><figcaption>自ら気づき、自ら考え、<br>自ら行動する</figcaption></figure></li>
           <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_02.png" alt="信頼・尊敬・感謝"><figcaption>相手に対する信頼・尊敬・<br>感謝を忘れず協力しあう</figcaption></figure></li>
-          <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_03.png" alt="チャレンジ"><figcaption>常に変化を恐れずに挑戦を続け、事業拡大のエンジンとなる</figcaption></figure></li>
+          <li><figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/value/img_value_03.png" alt="チャレンジ"><figcaption>常に変化を恐れずに挑戦を<br>続け、事業拡大のエンジン<br>となる</figcaption></figure></li>
         </ul>
       </div>
     </div>
