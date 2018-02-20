8ff6b55dce9d8a4044b4146a51d9f76c1eae48b1
PACTReePC <ohta@pactree.jp>
2018-02-20 18:45:41 +0900

    ページネーション記述をfunctionsに移動

--- functions.php
+++ functions.php
@@ -147,6 +147,62 @@ if ( ! function_exists( 'my_theme_default_setup' ) ) :
   function new_excerpt_more($more) {
     return '…';
   }
+  
+  //Pagenation
+  function pagination($pages = '', $range = 10) {
+    $showitems = ($range * 2) + 1; //表示するページ数（５ページを表示）
+
+    global $paged; //現在のページ値
+    if (empty($paged)) {
+      $paged = 1;
+    }
+  //デフォルトのページ
+
+    if ($pages == '') {
+      global $wp_query;
+      $pages = $wp_query->max_num_pages; //全ページ数を取得
+      if (!$pages) //全ページ数が空の場合は、１とする
+      {
+        $pages = 1;
+      }
+    }
+
+    if (1 != $pages) //全ページが１でない場合はページネーションを表示する
+    {
+      echo "<div class=\"pagenation\">\n";
+      echo "<ul>\n";
+      //Prev：現在のページ値が１より大きい場合は表示
+      if ($paged > 1) {
+        echo "<li class=\"prev\"><a href='" . get_pagenum_link($paged - 1) . "'>Prev</a></li>\n";
+      }
+
+      for ($i = 1; $i <= $pages; $i++) {
+        if (1 != $pages && (!($i >= $paged + $range + 1 || $i <= $paged - $range - 1) || $pages <= $showitems)) {
+          //三項演算子での条件分岐
+          echo ($paged == $i) ? "<li class=\"active\">" . $i . "</li>\n" : "<li><a href='" . get_pagenum_link($i) . "'>" . $i . "</a></li>\n";
+        }
+      }
+      //Next：総ページ数より現在のページ値が小さい場合は表示
+      if ($paged < $pages) {
+        echo "<li class=\"next\"><a href=\"" . get_pagenum_link($paged + 1) . "\">Next</a></li>\n";
+      }
+
+      echo "</ul>\n";
+      echo "</div>\n";
+    }
+  }
+
+  //ページネーション404対策
+  function custom_query($query) {
+    if (is_admin() || !$query->is_main_query()) {
+      return;
+    }
+    if ($query->is_archive('joblist') || $query->is_tax('cat_joblist')) {
+      $query->set('posts_per_page', 8); //10記事毎にページング
+      return;
+    }
+  }
+  add_action('pre_get_posts', 'custom_query');
 
   //トップページを固定ページとした時$pagedが取得されない問題を解決
   /*
@@ -158,7 +214,6 @@ if ( ! function_exists( 'my_theme_default_setup' ) ) :
   */
 
   //Authorアーカイブページを無効にする
-  /*
   add_action( 'template_redirect', 'theme_slug_redirect_author_archive' );
   function theme_slug_redirect_author_archive() {
       if (is_author() ) {
@@ -166,7 +221,6 @@ if ( ! function_exists( 'my_theme_default_setup' ) ) :
           exit;
       }
   }
-  */
 
   //---------------------
   // バックエンドUI
