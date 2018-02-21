a3cc09600be30156ff3a5bdf24a6551e2b5dc674
PACTReePC <ohta@pactree.jp>
2018-02-21 20:54:15 +0900

    「ぱどの人々」系のテンプレート、ファイル名変更

---
+++ archive-people.php
@@ -0,0 +1,199 @@
+<?php
+/*
+Template Name: ぱどの人々
+*/
+?>
+<?php get_header(); ?>
+<article id="people" class="underlayer">
+  <section id="sec_02" class="sec">
+    <h2 class="ttl_en-ja align_center">
+        <span class="en">PADO&nbsp;PEOPLE</span>
+        <span class="ja">ぱどの人々</span>
+      </h2>
+      <div class="desc align_center">
+      </div>
+
+  </section>
+  <section id="sec_03" class="sec">
+    <ul class="category">
+      <li><a href="#">営業</a></li>
+      <li><a href="#">営業事務</a></li>
+      <li><a href="#">配布スタッフ</a></li>
+      <li><a href="#">編集スタッフ</a></li>
+      <li><a href="#">商材管理</a></li>
+    </ul>
+    <ul class="tag">
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+    </ul>
+    <ul class="people_area">
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_light_blue">営業</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_yellow">配布スタッフ</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_green">商材管理</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_pink">営業事務</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_yellow">編集スタッフ</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_green">商材管理</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_blue">営業</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_yellow">配布スタッフ</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_green">商材管理</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_blue">営業</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_yellow">配布スタッフ</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_01.jpg" alt="">
+          <figcaption class="people_ttl">RIZAPグループとなった今も社員3人の時代から変わらないもの</figcaption>
+          <span class="bg_pp_green">商材管理</span>
+        </figure>
+        <h3>
+          <span class="neme">谷口&nbsp;夕貴</span>
+          <span class="occupations">営業部/平成23年4月入社</span>
+        </h3>
+        </a>
+      </li>
+    </ul>
+    <div class="cont">
+      <div>
+      </div>
+    </div>
+  </section>
+  </article>
+  <?php get_template_part( 'template-parts/content-entry' ); ?>
+<?php get_footer(); ?>
---
+++ single-people.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Template Name: ぱどの人々（下層）
+*/
+?>
+<?php get_header(); ?>
+<article id="people_02" class="underlayer">
+  <section id="sec_01" class="sec">
+    <div class="cover">
+
+    </div>
+  </section>
+  <section id="sec_02" class="sec">
+    <div class="people_area">
+        <div class="bg_pp_light_blue">
+          <h2>
+            <span class="name">谷口&nbsp;夕貴</span>
+            <span class="name_en">YUKI TANIGUCHI</span>
+          </h2>
+          <p class="occupations">営業部 / 平成23年4月入社</p>
+          <p>自己紹介文があればはいる自己紹介文があればはいる自己紹介文があればはいる自己紹介文があればはいる自己紹介文があればはいる自己紹介文があればはいる自己紹介文があれば</p>
+        </div>
+
+      </div>
+    <h2 class="align_center">
+        <span class="people_ttl">RIZAPグループとなった今も<br class="pc_br">創業当時から<br>変わらないもの</span>
+      </h2>
+
+  </section>
+  <section id="sec_03" class="sec bg_gray_02">
+    <h3 class="midashi">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</h3>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <div class="col_2-img_l clearfix">
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_02.jpg" alt=""></figure>
+        <div>
+          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </div>
+      </div>
+      <div class="col_2-img_r clearfix">
+        <div>
+          <h3 class="komidashi">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキス</h3>
+        <p>トテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキス</p>
+        </div>
+        <figure><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/people/img_people_03.jpg" alt=""></figure>
+      </div>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <div class="keyword">
+        <ul>
+        <li><a href="#">やりがい</a></li>
+        <li><a href="#">新卒</a></li>
+        <li><a href="#">中途</a></li>
+        <li><a href="#">女性</a></li>
+      </ul>
+      </div>
+      <ul class="category">
+        <li><a href="#">営業</a></li>
+        <li><a href="#">営業事務</a></li>
+        <li><a href="#">配布スタッフ</a></li>
+        <li><a href="#">編集スタッフ</a></li>
+        <li><a href="#">商材管理</a></li>
+      </ul>
+      <ul class="tag">
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+      <li><a href="#">女性</a></li>
+      <li><a href="#">やりがい</a></li>
+      <li><a href="#">新卒</a></li>
+      <li><a href="#">中途</a></li>
+    </ul>
+
+  </section>
+  <section id="sec_04" class="sec">
+    <div class="cont">
+      <h2 class="ttl_en-ja align_center">
+        <span class="en">RECOMMEND</span>
+        <span class="ja">おすすめの記事</span>
+      </h2>
+      <div>
+      </div>
+    </div>
+  </section>
+  </article>
+  <?php get_template_part( 'template-parts/content-entry' ); ?>
+<?php get_footer(); ?>
